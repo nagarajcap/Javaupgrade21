3dee37e25266ecae77b4f796a8b1664729fbc931
nagarajcap <164017641+nagarajcap@users.noreply.github.com>
2024-10-13 18:32:51 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -6,7 +6,8 @@ REF="master"                             # Branch reference to trigger the actio
 
 # Trigger GitHub Action via GitHub API
 curl -X POST \
-  -H "Accept: application/vnd.github.v3+json" \
-  -H "Authorization: token $GITHUB_TOKEN" \
+  -H "Accept: application/vnd.github+json" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  -H "Authorization: Bearer $GITHUB_TOKEN \
   https://api.github.com/repos/$GITHUB_REPO/actions/workflows/$GITHUB_WORKFLOW/dispatches \
   -d '{"ref":"'"$REF"'"}'
