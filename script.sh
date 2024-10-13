5fa4420b8bf9f9516d5f940db76ef29c4541184d
nagarajcap <164017641+nagarajcap@users.noreply.github.com>
2024-10-13 18:59:31 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 GITHUB_REPO="nagarajcap/Javaupgrade21"  # Replace with your GitHub repo
-GITHUB_TOKEN="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"    # GitHub token with repo access
+GITHUB_TOKEN="622AHWqjGdvlE9p122ZrZ6NnB7WZpF0ttSIG"    # GitHub token with repo access
 GITHUB_WORKFLOW="jmeterui.yml"      # Replace with the exact workflow filename
 REF="master"                             # Branch reference to trigger the action
 
@@ -8,6 +8,6 @@ REF="master"                             # Branch reference to trigger the actio
 curl -X POST \
   -H "Accept: application/vnd.github+json" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
-  -H "Authorization: Bearer $GITHUB_TOKEN" \
+  -H "Authorization: Bearer ghp_$GITHUB_TOKEN" \
   https://api.github.com/repos/$GITHUB_REPO/actions/workflows/$GITHUB_WORKFLOW/dispatches \
   -d '{"ref":"'"$REF"'"}'
